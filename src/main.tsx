1b37c842a3b81368c956c5e5bd34a8522000d075
Kjollen <olgaavlasova@ya.ru>
2026-09-23 14:37:38 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,6 @@ import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
 
-
-+++ src/main.tsx (修改后)
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App.tsx";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
-
 // Register service worker for PWA
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
