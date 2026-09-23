0f70fb15738e4bc6223f2277aea531c725697fcd
Kjollen <olgaavlasova@ya.ru>
2026-09-23 19:33:07 +0300

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -12,5 +12,24 @@ export interface YarnItem {
   notes: string;
   photo: string;
   quantity: number;
+  shop: string;
+  orderNumber: string;
+  pricePerGram: string;
+  totalPrice: string;
+  dateAdded: string;
+}
+
+export interface Project {
+  id: string;
+  yarnItemId: string;
+  name: string;
+  photo: string;
+  yarnUsedWeight: string;
+  needleSize: string;
+  pattern: string;
+  notes: string;
+  startDate: string;
+  endDate: string;
+  status: 'in-progress' | 'completed' | 'planned';
   dateAdded: string;
 }
